8f5e24e141ba793bc872f0da0b08e3e8c9ef8cd0
Sergey Burmisov <skypath@gmail.com>
2015-12-04 19:24:42 +0300

    force update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import db from './db';
 import mkdirp from 'mkdirp';
 import { run } from './runner';
 import iisBaseUrl from 'iis-baseurl';
+import updateReports from './updateReports';
 
 const FILES_PATH = path.resolve(process.cwd(), 'data/files');
 mkdirp.sync(FILES_PATH);
@@ -26,6 +27,14 @@ app.get('/reports/:truckId', (req, res) => {
     if (err) { throw err; }
     if (!doc) { return res.status(404).send('Нет данных'); }
     res.download(path.resolve(FILES_PATH, doc.fileName), doc.originalName);
+  });
+});
+
+app.post('/reports/reload', (req, res) => {
+  updateReports.then(numUpdated => {
+    res.json({ numUpdated });
+  }).catch(err => {
+    throw err;
   });
 });
 
